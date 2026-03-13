b2981db8834d71bcf9eb72bf12a85a51dc5f3846
MUKILTEO\1717482 <1717482@Mukilteo.Wednet.Edu>
2026-03-13 08:57:15 -0700

    no message

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -21,11 +21,11 @@ public class PlayerCam : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-
+        //float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
 
-        //xRotation -= mouseY;
+       //xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
