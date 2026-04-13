e0c3f8fc86a27c17da8490b3388a10187c7adf6f
MUKILTEO\1717482 <1717482@Mukilteo.Wednet.Edu>
2026-04-13 10:00:03 -0700

    Shop voicelines with implementation.

---
+++ Assets/curtis/Scripts/Number Generator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour, IInteractable
+{
+    public GameObject shopUI;
+    public AudioSource shop;
+    public AudioClip[] randomOpening;
+    public AudioClip[] randomClosing;
+
+    public void Interact()
+    {
+        if (shopUI == null)
+            return;
+
+        if (shopUI.activeSelf == false)
+        {
+            ActivateObject();
+        }
+        else
+        {
+            DeactivateObject();
+        }
+    }
+
+
+    public void ActivateObject()
+    {
+        shopUI.SetActive(true);
+        shop.PlayOneShot(randomOpening[Random.Range(0, randomOpening.Length)]);
+    }
+
+    public void DeactivateObject()
+    {
+        shopUI.SetActive(false);
+        shop.PlayOneShot(randomClosing[Random.Range(0, randomClosing.Length)]);
+    }
+}
